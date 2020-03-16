0b1c3a1f3d0c30c80cbcd634e61bf7efc6b493ed
cagarci <49324580+cagarci@users.noreply.github.com>
2020-03-15 18:03:27 -0700

    Add files via upload

---
+++ GccApplication10/adc.h
@@ -0,0 +1,10 @@
+// Code writtten by Avinash at extremeelectronics 
+//https://extremeelectronics.co.in/avr-tutorials/interfacing-analog-joystick-with-avr-atmega32/
+
+#ifndef ADC_H
+#define ADC_H
+
+void InitADC();
+uint16_t ReadADC(uint8_t ch);
+
+#endif
